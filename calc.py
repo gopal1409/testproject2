63e5facbe22c71ddaf0ebbd6e65a9f4ca063f7e8
root <root@ip-172-31-16-36.us-east-2.compute.internal>
2018-05-02 16:12:45 +0000

    intial commit

---
+++ calc.py
@@ -0,0 +1,20 @@
+
+def add(x, y):
+    """Add Function"""
+hjhkhhk    return x + y
+
+
+def subtract(x, y):
+    """Subtract Function"""
+    return x - y
+
+
+def multiply(x, y):
+    """Multiply Function"""
+    return x * y
+
+
+def divide(x, y):
+    """Divide Function"""
+    return x / y
+
